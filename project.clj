fcd7d2ae58a32375f4e837972f8fa7854b0900e8
Timo Geusch <timo@unix-consult.com>
2021-09-12 10:46:15 -0400

    Improved project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-pem-decoder "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Very simple decoder for basic PEM files that can handle certificates and private PKCS8 keys"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
